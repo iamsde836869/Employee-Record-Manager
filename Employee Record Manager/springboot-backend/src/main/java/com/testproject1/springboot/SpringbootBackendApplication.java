dc9466cc90f95f885d64997e9c28c967850f7301
iamsde836869 <iamsde836869@gmail.com>
2021-02-06 15:54:10 +0530

    Final Commit 1

---
+++ Employee Record Manager/springboot-backend/src/main/java/com/testproject1/springboot/SpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.testproject1.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendApplication.class, args);
+	}
+
+}
